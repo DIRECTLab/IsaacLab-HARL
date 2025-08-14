2b28336959c395184a5a44ca4060efceb7da5f39
zenif-night <christophertaylorallred@gmail.com>
2025-08-14 17:27:48 -0600

    set up copy form Adv

--- source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent/__init__.py
@@ -12,6 +12,7 @@ import gymnasium as gym
 from . import agents
 from .anymal_c_multi_agent import AnymalCMultiAgentBar, AnymalCMultiAgentFlatEnvCfg
 from .anymal_c_multi_agent_adversarial import AnymalCAdversarialEnv, AnymalCAdversarialEnvCfg
+from .anymal_c_multi_agent_adversarial_soccer import AnymalCAdversarialSoccerEnv, AnymalCAdversarialSoccerEnvCfg
 from .anymal_c_multi_agent_adversarial_single_agent import AnymalCAdversarialSingleAgentEnv, AnymalCAdversarialSingleAgentEnvCfg
 from .anymal_c_multi_agent_adversarial_sumo_stage1 import AnymalCAdversarialSumoStage1Env, AnymalCAdversarialSumoStage1EnvCfg
 from .anymal_c_multi_agent_adversarial_sumo_stage2 import AnymalCAdversarialSumoStage2Env, AnymalCAdversarialSumoStage2EnvCfg
@@ -46,6 +47,21 @@ gym.register(
         "harl_happo_adv_cfg_entry_point": f"{agents.__name__}:harl_happo_adv_cfg.yaml"
     },
 )
+
+gym.register(
+    id="Isaac-Multi-Agent-Flat-Anymal-C-Direct-Adversarial-Soccer-v0",
+    entry_point=AnymalCAdversarialSoccerEnv,
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": AnymalCAdversarialSoccerEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_flat_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCFlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+        "harl_happo_cfg_entry_point": f"{agents.__name__}:harl_happo_cfg.yaml",
+        "harl_happo_adv_cfg_entry_point": f"{agents.__name__}:harl_happo_adv_cfg.yaml"
+    },
+)
+
 
 
 gym.register(

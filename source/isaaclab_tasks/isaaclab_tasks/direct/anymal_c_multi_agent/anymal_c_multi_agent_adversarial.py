48dd11ef35a5310a3139c2a62959916fe8549242
Isaac Peterson <isaac.peterson@usu.edu>
2025-08-14 13:32:51 -0600

    added task, and fixed multi agent adversarial env

--- source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent/anymal_c_multi_agent_adversarial.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent/anymal_c_multi_agent_adversarial.py
@@ -56,6 +56,28 @@ class EventCfg:
         },
     )
 
+    physics_material_1 = EventTerm(
+        func=mdp.randomize_rigid_body_material,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot_1", body_names=".*"),
+            "static_friction_range": (0.8, 0.8),
+            "dynamic_friction_range": (0.6, 0.6),
+            "restitution_range": (0.0, 0.0),
+            "num_buckets": 64,
+        },
+    )
+
+    add_base_mass_1 = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot_1", body_names="base"),
+            "mass_distribution_params": (-5.0, 5.0),
+            "operation": "add",
+        },
+    )
+
 
 def define_markers() -> VisualizationMarkers:
     marker_cfg = VisualizationMarkersCfg(

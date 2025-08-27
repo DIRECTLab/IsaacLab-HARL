80872a33f1b26caf5d78766b1270c316596cbf7f
Jacob Morrey <jacob.g.morrey@gmail.com>
2025-08-27 13:07:04 -0600

    Separated envs for pushing blocks and full observation walking

--- source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent/__init__.py
@@ -20,6 +20,7 @@ from .sumo_stage_1_single_agent import SumoStage1EnvSingleAgent, SumoStage1EnvSi
 from .anymal_c_multi_agent_adversarial_same_team import AnymalCAdversarialSameTeamEnv, AnymalCAdversarialSameTeamEnvCfg
 from .anymal_c_multi_agent_bar_same_team import AnymalCMultiAgentFlatSameTeamBarEnv, AnymalCMultiAgentFlatSameTeamBarEnvCfg
 from .sumo_stage_1_blocks import SumoStage1BlocksEnv, SumoStage1BlocksEnvCfg
+from .sumo_stage_1_blocks_push import SumoStage1BlocksPushEnv, SumoStage1BlocksPushEnvCfg
 from .sumo_stage_1_blocks_full_obs import SumoStage1BlocksFullEnv, SumoStage1BlocksFullEnvCfg
 
 ##
@@ -160,6 +161,20 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": SumoStage1BlocksEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_flat_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCFlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+        "harl_happo_cfg_entry_point": f"{agents.__name__}:harl_happo_cfg.yaml",
+        "harl_happo_adv_cfg_entry_point": f"{agents.__name__}:harl_happo_adv_cfg.yaml"
+    },
+)
+
+gym.register(
+    id="Isaac-Multi-Agent-Flat-Sumo-Stage1-Blocks-Push-v0",
+    entry_point=SumoStage1BlocksPushEnv,
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": SumoStage1BlocksPushEnvCfg,
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_flat_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCFlatPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",

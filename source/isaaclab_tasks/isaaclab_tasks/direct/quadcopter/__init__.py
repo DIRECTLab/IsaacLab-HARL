2f9ac5811b195eba6b9d387241a4d7bd5ff6bfde
zenif-night <christophertaylorallred@gmail.com>
2025-09-02 22:20:54 -0600

    set up drone

--- source/isaaclab_tasks/isaaclab_tasks/direct/quadcopter/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/quadcopter/__init__.py
@@ -39,3 +39,16 @@ gym.register(
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
 )
+
+gym.register(
+    id="Isaac-Quadcopter-Direct-Team-HAPPO-v0",
+    entry_point=f"{__name__}.quadcopter_env_happo_stage_1:QuadcopterMARLEnvTeam",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.quadcopter_env_happo_stage_1:QuadcopterMARLEnvTeamCfg",
+        "harl_happo_adv_cfg_entry_point": f"{agents.__name__}:harl_happo_adv_cfg.yaml",
+        # "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        # "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:QuadcopterPPORunnerCfg",
+        # "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)

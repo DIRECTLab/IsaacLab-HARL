48011001f908d8d901a34789c2aae69d97d211a6
Jacob Morrey <jacob.g.morrey@gmail.com>
2025-09-05 17:21:09 -0600

    Added anymal c soccer environment and running env

--- source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent_soccer/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/anymal_c_multi_agent_soccer/__init__.py
@@ -12,6 +12,8 @@ import gymnasium as gym
 from . import agents
 from .anymal_c_soccer_find_ball_teacher import AnymalCFindBallHappoEnv, AnymalCHappoFindBallEnvCfg
 from .leatherback_anymal_soccer_stage_2 import AnymalCPlaySoccer, AnymalCSoccerCfg
+from .anymal_c_soccer_running import AnymalSoccerRunningEnv, AnymalSoccerRunningEnvCfg
+from .anymal_c_soccer_stage_1 import AnymalStage1SoccerEnv, AnymalStage1SoccerEnvCfg
 ##
 # Register Gym environments.
 ##
@@ -42,3 +44,28 @@ gym.register(
     },
 )
 
+gym.register(
+    id="AnymalC_Soccer_Soccer_Running-v0",
+    entry_point=AnymalSoccerRunningEnv, 
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": AnymalSoccerRunningEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_flat_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCSoccerFlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+        "harl_happo_cfg_entry_point": f"{agents.__name__}:harl_happo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="AnymalC_Soccer_Stage1-v0",
+    entry_point=AnymalStage1SoccerEnv, 
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": AnymalStage1SoccerEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_flat_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCSoccerFlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+        "harl_happo_cfg_entry_point": f"{agents.__name__}:harl_happo_cfg.yaml",
+    },
+)
